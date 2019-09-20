354c293ce521b3ff290878055c1fc6c685ffd241
Marc Blanchard <marcblanchard0930@gmail.com>
2019-09-20 18:04:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 2, "Male", "Brown", "Cool", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, "Male", "Black", "Rowdy", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, "Male", "Brown", "Cool", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, "Male", "Brown", "Cool", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 6, "Male", "Brown", "Chill", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, "Male", "Grey", "Angry", false);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 8, "Male", "Brown", "Cool", true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 9, "Male", "White", "Axious", false);
